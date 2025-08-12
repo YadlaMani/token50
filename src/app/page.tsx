486a2beb4efd32bd531ca9f88cb0fd5a4138d5fd
YadlaMani <yadlamaniymn2005@gmail.com>
2025-08-12 21:30:05 +0530

    added wallet

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+// import { cookieStorage, createStorage, http } from '@wagmi/core'
+import { ConnectButton } from "@/components/ConnectButton";
+
+export default function Home() {
+  return (
+    <div>
+      <ConnectButton />
+    </div>
+  );
+}
